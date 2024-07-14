1ff0c6188dad144dcdefaa6d37f107906a22ae8d
q1p1 <meshari.algahbban@wakecap.com>
2024-07-15 01:20:02 +0300

    ul and ol list

--- src/Prent.jsx
+++ src/Prent.jsx
@@ -1,9 +1,30 @@
-function Prent(){
-return(
+function Prent() {
+  return (
     <div>
-        <h1>Meshari</h1>
+      <ul>
+        <li>B</li>
+        <ol type="1">
+          <li>B1</li>
+          <li>B2</li>
+          <ul>
+            <li>B2a</li>
+            <ul>
+              <li>B2aa</li>
+              <li>B2ab</li>
+            </ul>
+            <li>B2b</li>
+            <li>B2c</li>
+          </ul>
+          <li>B3</li>
+          <ol>
+            <li>B31</li>
+            <li>B32</li>
+          </ol>
+        </ol>
+
+        <li>c</li>
+      </ul>
     </div>
-    
-);
+  );
 }
-export default Prent
+export default Prent;
