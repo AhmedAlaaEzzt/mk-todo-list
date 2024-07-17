093ee39fcb5f8311dbddde2d96340384b5e8abe0
q1p1 <meshari.algahbban@wakecap.com>
2024-07-17 15:45:54 +0300

    amendment

--- src/components/card/CardTask.tsx
+++ src/components/card/CardTask.tsx
@@ -1,14 +1,10 @@
-import todo from "../assets/to-do-list.png";
-
 import "./card.css";
 function CardTask() {
   return (
     <div style={{ textAlign: "center", padding: "15%" }}>
       <div className="container">
         <div className="dolist">
-          <h2>
-            ToDo <img src={todo} alt="To-Do List" className="img-todo" />
-          </h2>
+          <h2>ToDo</h2>
           <div className="row">
             <input type="text" id="input-box" placeholder="Enter your task" />
           </div>
