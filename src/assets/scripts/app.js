54d805d8f9568a3be410e1e5fa15ad7a881a2030
q1p1 <meshari.algahbban@wakecap.com>
2024-07-24 12:48:19 +0300

    create a function outpuut

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,6 +1,17 @@
-function add(nmber1, number2) {
-  const result = nmber1 + number2;
-  return result;
+const numberv1 = document.getElementsByName("input1")[0];
+const numberv2 = document.getElementsByName("input2")[0];
+const addButton = document.getElementById("addButton");
+const outputDiv = document.getElementById("output");
+
+function add() {
+  const num1 = parseFloat(numberv1.value);
+  const num2 = parseFloat(numberv2.value);
+  const result = num1 + num2;
+  output(result);
 }
-const addResult = add(5, 8);
-alert(addResult);
+
+function output(result) {
+  outputDiv.textContent = result;
+}
+
+addButton.addEventListener("click", add);
