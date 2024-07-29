761c35149345e7b41da7c4db6870a86d02cc81b9
q1p1 <meshari.algahbban@wakecap.com>
2024-07-29 20:58:29 +0300

    [FE] WorkforceItem Sprint 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <>starting point</>;
-}
+import React, { useState } from "react";
+import WorkforceItem from "./components/ItemStatus";
+
+const App: React.FC = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+    >
+      <WorkforceItem isHovered={isHovered} isActive={isClicked} />
+    </div>
+  );
+};
 
 export default App;
