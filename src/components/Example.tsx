07308782e43a08dfedd3cad39499d9d58dec47f9
q1p1 <meshari.algahbban@wakecap.com>
2024-07-29 12:05:35 +0300

    [FE] Workforce Legends Sprint 4 task

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,14 +1,20 @@
 function Example() {
   return (
     <div className="bg-white p-4 rounded shadow-md w-64">
-      <p className="text-gray-500 mb-2">Legends:</p>
+      <p className="text-gray-500 mb-2">Workforce Legends:</p>
       <div className="flex items-center bg-gray-50 p-2 rounded mb-2">
-        <div className="w-4 h-4 bg-green-500 rounded mr-2"></div>
+        <div className="w-4 h-4 bg-green-500 rounded-full mr-2"></div>
         <span className="text-gray-700">Active</span>
       </div>
       <div className="flex items-center bg-gray-50 p-2 rounded">
-        <div className="w-4 h-4 bg-orange-500 rounded mr-2"></div>
+        <div className="w-4 h-4 bg-orange-500 rounded-full mr-2"></div>
         <span className="text-gray-700">Inactive</span>
+      </div>
+      <div className="bg-[#E9F5FE] p-5 w-19 mt-3 rounded shadow-md ">
+        <p className="text-xs">
+          Online workers in the last <span className="font-bold">5 mins</span>{" "}
+          are shown on the map.
+        </p>
       </div>
     </div>
   );
