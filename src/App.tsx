40458a55471c1dc50c83ef9f2e4b79d9ab3fb914
q1p1 <meshari.algahbban@wakecap.com>
2024-08-06 14:13:12 +0300

    add a Point in a Layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import { ArcgisMap } from "@arcgis/map-components-react";
-import './App.css'; // تأكد من استيراد ملف CSS
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Point from "@arcgis/core/geometry/Point";
+import "./App.css";
+import Graphic from "@arcgis/core/Graphic";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
 
 function App() {
   return (
     <div className="MapDev">
-      <ArcgisMap ></ArcgisMap>
+      <ArcgisMap
+        center={[36.47618573252876, 28.4044846389379]}
+        zoom={17}
+        onArcgisViewReadyChange={(event) => {
+          const map = event.target.map;
+          const graphicsLayer = new GraphicsLayer();
+          map.add(graphicsLayer);
+          const point = new Point({
+            longitude: 36.47618573252876,
+            latitude: 28.4044846389379,
+          });
+          const simpleMarkerSymbol = new SimpleMarkerSymbol({
+            color: "red",
+          });
+          const graphic = new Graphic({
+            geometry: point,
+            symbol: simpleMarkerSymbol,
+          });
+
+          graphicsLayer.add(graphic);
+        }}
+      ></ArcgisMap>
     </div>
   );
 }
