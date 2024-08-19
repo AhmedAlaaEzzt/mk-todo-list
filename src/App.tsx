9c0e2dd090c57fcf6382b7e7a707e375b6b1ce27
q1p1 <meshari.algahbban@wakecap.com>
2024-08-19 14:57:41 +0300

    Change a floor add a feature watch and a new file name`s (widgetSpas.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
 import template from "./components/popup";
 import "./App.css";
 import throttle from "lodash/throttle";
-import MapView from "@arcgis/core/views/MapView";
 
 const simpleMarkerSymbol = new SimpleMarkerSymbol({
   color: "#F97316",
@@ -27,22 +26,11 @@ function App() {
   const [pointCount, setPointCount] = useState(0);
   const [mapView, setMapView] = useState<__esri.MapView>();
 
-  /*
-  We used useEffect here to ensure that once the data is successfully loaded from the mapView, 
-  it triggers the fetching of data from the JSON file and then displays that data in the console.
-  */
   useEffect(() => {
     const fetchData = async () => {
       if (mapView) {
         try {
-          // Fetch data from the local JSON file
           const response = await fetch("./MockData.json");
-          /*
-          We used the await keyword here to ensure that the operation does not pause or delay while waiting for data
-           to be fetched from the JSON file. Instead, it allows the code to continue running asynchronously
-            until the data is fully retrieved and available for use. This way, the data appears without any
-             unnecessary delays.
-          */
           const data = await response.json();
           console.log("Data from MockData.json:", data);
 
@@ -61,7 +49,6 @@ function App() {
 
           mapView.map.add(geoJsonLayer);
 
-          // Adding throttled watch for extent
           const throttledUpdate = throttle(() => {
             geoJsonLayer.queryFeatures().then((result) => {
               const visibleFeatures = result.features.filter((feature) => {
@@ -75,27 +62,12 @@ function App() {
 
               setPointCount(visibleFeatures.length);
             });
-          }, 200); // 200ms delay for throttling
+          }, 200);
 
           mapView.watch("extent", throttledUpdate);
 
           const graphicsLayer = new GraphicsLayer();
           mapView.map.add(graphicsLayer);
-          /* 
-          Add your point and graphic to the graphics layer if needed
-          const point = new Point({
-            longitude: 36.47618573252876,
-            latitude: 28.4044846389379,
-          });
-
-          const graphic = new Graphic({
-            geometry: point,
-            symbol: simpleMarkerSymbol,
-          });
-
-          graphicsLayer.add(graphic);
-
-          */
         } catch (error) {
           console.error("Error", error);
         }
@@ -110,7 +82,7 @@ function App() {
       <div className="MapDev relative">
         <ArcgisMap
           basemap={"hybrid"}
-          center={[36.47618573252876, 28.4044846389379]}
+          // center={[36.47618573252876, 28.4044846389379]}
           zoom={18}
           onArcgisViewReadyChange={(event) => {
             const { view } = event.target;
@@ -149,7 +121,7 @@ function App() {
         {showLegends && <WorkforceLegends id="WorkforceLegends" />}
         {showWorkforce && (
           <div className="flex absolute top-4 left-[80px] space-x-4">
-            <Workforce id="Workforcen" view={new MapView} />
+            {mapView && <Workforce id="Workforce" view={mapView} />}
           </div>
         )}
       </div>
